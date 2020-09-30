dd01c595eaac3ca810d6488657a6ba9e8e8d4964
Jonas De Corte <jonas.decorte@student.hogent.be>
2020-09-30 11:26:35 +0200

    navigeren en doorgeven van param

--- Lab2Layout/BlankPage1.xaml.cs
+++ Lab2Layout/BlankPage1.xaml.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.UI.Popups;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -13,26 +14,26 @@ using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
 
-// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
 
 namespace Lab2Layout
 {
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
-    public sealed partial class MainPage : Page
+    public sealed partial class BlankPage1 : Page
     {
-        public MainPage()
+        public BlankPage1()
         {
             this.InitializeComponent();
         }
-
-        private void Button_Click(object sender, RoutedEventArgs e)
+        protected override void OnNavigatedTo(NavigationEventArgs e)
         {
-
-            // navigeren naar andere pagina en eventueel data doorgeven
-            Frame.Navigate(typeof(BlankPage1), "Param van mainpage");
-
+            base.OnNavigatedTo(e);
+            // tonen dat we de param doorgeven van mainpage
+            var param = e.Parameter.ToString();
+            MessageDialog md = new MessageDialog($"{param}");
+            md.ShowAsync();
         }
     }
 }
